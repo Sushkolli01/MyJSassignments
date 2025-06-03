9d436c8a19b2199abde7efcc8bd91123a83ab316
Sushkolli01 <151184683+Sushkolli01@users.noreply.github.com>
2025-06-03 16:34:13 +0100

    Update 03-stringAnagram.js

--- Week2/03-stringAnagram.js
+++ Week2/03-stringAnagram.js
@@ -3,11 +3,8 @@
 function stringtolowercase(string01,string02, callback){
 
     const lowerstr1 = string01.toLowerCase();
-    const lowerstr2 = string02.toLowerCase();
-    
-    callback(lowerstr1,lowerstr2);
-
-    
+    const lowerstr2 = string02.toLowerCase();    
+    callback(lowerstr1,lowerstr2);    
 }
 function stringAnagramCheck(string01,string02){
 
@@ -18,23 +15,17 @@ function stringAnagramCheck(string01,string02){
 
             if (string02.includes(stringarray1[i])) {
                 if(i === stringarray1.length-1){
-                console.log(`-----Strings "${string01} ${string02}" are Anagram-----`);  
-                //console.log("Charc value:"+stringarray1[i]);
-                 
-                }
-                             
+                console.log(`-----Strings "${string01} ${string02}" are Anagram-----`);                     
+                }                             
             }
             else {
                 console.log("------Strings are not Anagram-----");
                 break;
-            }
-           
-        }
-        
+            }           
+        }        
     } else {
         console.log("String length doesnt matches");
     }
-
 }
 //stringAnagramCheck("Hello","World");
 stringtolowercase("Triangle","Integral",stringAnagramCheck);
